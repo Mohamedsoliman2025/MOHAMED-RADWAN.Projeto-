1cf18b08aa254f611d5f7f40145abfbc8f46f668
Mohamedsoliman2025 <modamrm2022@gmail.com>
2025-11-09 10:41:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, flash
 from task_controller import TaskController
 
-app = Flask(__name__)
+app = Flask(_name_)
+app.secret_key = "segredo"
+controller = TaskController()
 
-# Rota principal (renderiza o front-end)
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    tasks = controller.get_all_tasks()
+    return render_template("index.html", tasks=tasks)
 
-# Rotas da API REST
-@app.route('/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify(TaskController.get_all_tasks())
-
-@app.route('/tasks', methods=['POST'])
+@app.route("/add", methods=["POST"])
 def add_task():
-    data = request.get_json()
-    return jsonify(TaskController.add_task(data["title"]))
+    title = request.form["title"]
+    controller.add_task(title)
+    flash("Tarefa adicionada com sucesso!")
+    return redirect("/")
 
-@app.route('/tasks/<int:task_id>', methods=['PUT'])
-def update_task(task_id):
-    data = request.get_json()
-    return jsonify(TaskController.update_task(task_id, data["title"]))
-
-@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+@app.route("/delete/<int:task_id>", methods=["POST"])
 def delete_task(task_id):
-    return jsonify(TaskController.delete_task(task_id))
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    controller.delete_task(task_id)
+    flash("Tarefa removida com sucesso!")
+    return redirect("/")
